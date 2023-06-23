0f1b45b6fec0a96cca98d6f8603b1de7c71d8dd4
MrJuaum <74993031+MrJuaumBR@users.noreply.github.com>
2023-06-23 18:34:03 -0300

    Updated

---
+++ website/scripts.js
@@ -0,0 +1,45 @@
+var dataJSON = {
+    "free":[
+        {"style.css":"/data/style.css"},
+        {"memes-pt-br.json":"/data/memes-pt-br.json"}
+    ]
+}
+
+function getFiles(){
+    var d = document.getElementById("cnt");
+    while (d.firstChild) {
+        d.removeChild(d.firstChild);
+    }
+
+    var e =document.createElement("ol");
+    var l = dataJSON["free"]
+    /*/ 
+        var x = document.createElement("li");
+        x.innerText = element['memes-pt-br.json'];
+        e.appendChild(x);
+    /*/
+    for (var i =0;i< l.length; i++){
+        var obj = l[i];
+        for (var key in obj){
+            var v = obj[key];
+            var x = document.createElement("li");
+            var a = document.createElement("a");
+            a.href = "."+v;
+            a.innerText = key;
+            x.appendChild(a);
+            e.appendChild(x);
+        }
+    }
+    d.appendChild(e);
+}
+
+function showOrHide(){
+    var d = document.getElementById("cnt");
+    if (d.hidden){
+        d.hidden = false;
+        getFiles();
+    }
+    else{
+        d.hidden = true;
+    }
+}
