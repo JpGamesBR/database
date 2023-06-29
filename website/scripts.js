b5674bc2bff854023eb23cb1fce5cb0b81a0e6ca
MrJuaum <74993031+MrJuaumBR@users.noreply.github.com>
2023-06-29 15:04:45 -0300

    Updated
    View Logs for more.

--- website/scripts.js
+++ website/scripts.js
@@ -6,19 +6,51 @@ var dataJSON = {
         {"Ez Cheat.js":"/data/ezCheat.js"}
     ],
     "logs":[
-        {"date":"26/06/23 21:25","text":"Some improvments in JavaScript and CSS and a new 'go to top' button"},
+        {"date":"29/06/23 15:00","text":"Some improvements in JavaScript and CSS and new function to Show Content button."},
+        {"date":"26/06/23 21:25","text":"Some improvements in JavaScript and CSS and a new 'go to top' button"},
         {"date":"26/06/23 21:08","text":"Added log System Logic, and functionalities"},
         {"date":"26/06/23 17:47","text":"Added EzCheat.js, Added Log System and others..."}
     ]
 }
 
+function GetMemes(){
+    fetch("./data/memes-pt-br.json").then(response => {return response.json();}).then(data => console.log(data));
+}
+
+function GetClickedSpan(me){
+    // Get JSON File
+    var free = dataJSON["free"][me];
+    for (key in free){
+        var v = free[key];
+        var k = key;
+    }
+    // Get Frame of item
+    var iteminfo = document.getElementById("item-info");;
+    
+    var title = document.getElementById("item-title");
+    var link = document.getElementById("item-link");
+    var id = document.getElementById('item-id');
+
+    // Set Div values
+    title.textContent = "Title: " + k;
+    link.href= v;
+    id.textContent = "ID: "+me
+
+    // Show or hide
+    if (iteminfo.hidden || me){
+        iteminfo.hidden = false;
+    }
+    else{
+        iteminfo.hidden = true;
+    }
+}
+
 function getFiles(){
-    var d = document.getElementById("cnt");
-    while (d.firstChild) {
-        d.removeChild(d.firstChild);
+    var e =document.getElementById("cnt_list");
+    while (e.firstChild) {
+        e.removeChild(e.firstChild);
     }
 
-    var e =document.createElement("ol");
     var l = dataJSON["free"]
     /*/ 
         var x = document.createElement("li");
@@ -30,14 +62,24 @@ function getFiles(){
         for (var key in obj){
             var v = obj[key];
             var x = document.createElement("li");
-            var a = document.createElement("a");
-            a.href = "."+v;
+            var a = document.createElement("button");
+            a.style.background = "none";
+            a.className = "c-green";
             a.innerText = key;
+            a.Value = i;
+            a.id = "item-"+i;
             x.appendChild(a);
             e.appendChild(x);
         }
     }
-    d.appendChild(e);
+    
+    var childs = e.childNodes;
+    childs.forEach(function(item){
+        var val = item.firstChild.id.split('-')[1];
+        item.addEventListener('click',function(){
+            GetClickedSpan(val);
+        });
+    })
 }
 
 function getLogs(){
@@ -63,10 +105,7 @@ function showOrHide(id){
     var d = document.getElementById(id);
     if (d.hidden){
         d.hidden = false;
-        if (id == "cnt"){
-            getFiles();   
-        }
-        else if(id == "logs"){
+        if(id == "logs"){
             getLogs();
         };
     }
@@ -88,4 +127,23 @@ function scrollFunction() {
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function OverlayOpen(id){
+    var d = document.getElementById(id);
+    if (d.style.display=="none"){
+        d.style.display = "block";
+        if (id=="cnt"){
+            getFiles();
+        }
+    }
+    else{
+        if (d.style.display=="block"){
+            d.style.display = "none";
+        }
+        else{
+            d.style.display = "none"
+        };
+    };
+    
 }
